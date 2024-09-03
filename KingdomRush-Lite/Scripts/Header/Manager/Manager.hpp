65f56b4f329c04ceb6b8bc9f84ee6ecbed5513a8
WhythZ <jichenzong@outlook.com>
2024-09-03 11:43:47 +0800

    9.03 seperate header and source

--- KingdomRush-Lite/Scripts/Header/Manager/Manager.hpp
+++ KingdomRush-Lite/Scripts/Header/Manager/Manager.hpp
@@ -3,7 +3,7 @@
 #define _MANAGER_HPP_
 
 //可继承单例Manager模板类实现
-template<typename T>
+template <typename T>
 class Manager
 {
 protected:
@@ -12,17 +12,7 @@ protected:
 
 public:
 	//静态的函数，用于获取Manager类的唯一实例指针
-	static T* GetInstance()
-	{
-		//若manager未被创建（为空指针），则在堆区创建一个
-		if (!manager)
-		{
-			manager = new T();
-		}
-
-		//这样我们就可以在外部通过Manager* xxxx = Manager::GetInstance();获取内部这个Manager对象的地址，而不是创建一个新的Manager
-		return manager;
-	}
+	static T* GetInstance();
 
 protected:
 	//构造函数，单例模式的类不应当能被在外部创建对象
@@ -35,8 +25,21 @@ protected:
 	Manager& operator=(const Manager&) = delete;
 };
 
-//在外部初始化静态成员变量
-template<typename T>
+//初始化静态成员变量
+template <typename T>
 T* Manager<T>::manager = nullptr;
+
+template <typename T>
+T* Manager<T>::GetInstance()
+{
+	//若manager未被创建（为空指针），则在堆区创建一个
+	if (!manager)
+	{
+		manager = new T();
+	}
+
+	//这样我们就可以在外部通过Manager* xxxx = Manager::GetInstance();获取内部这个Manager对象的地址，而不是创建一个新的Manager
+	return manager;
+}
 
 #endif
--- KingdomRush-Lite/Scripts/Header/Route/Route.hpp
+++ KingdomRush-Lite/Scripts/Header/Route/Route.hpp
@@ -6,12 +6,14 @@
 #include "../Map/Tile.h"
 #include "../Map/Map.hpp"
 
+//存储行进路径上瓦片索引点的数组数据类型
+typedef std::vector<SDL_Point> TileIdxList;
+
 //记录怪物的行进路径，用于生成洋流图
 class Route
 {
 private:
-	//存储行进路径上瓦片索引点的数组数据类型
-	typedef std::vector<SDL_Point> TileIdxList;
+	//数组用于存储行进路径上瓦片索引点
 	TileIdxList tileIdxList;
 
 public:
@@ -19,83 +21,89 @@ public:
 	Route() = default;
 	
 	//传入瓦片地图进行怪物行进路径瓦片索引列表的生成，以第二参数传入的索引指定的单元格位置为起点开始寻路（怪物可能有多个出生点）
-	Route(const TileMap& _map, const SDL_Point& _beginIdx)
-	{
-		//存储瓦片地图的长宽
-		size_t _mapWidth = _map[0].size();
-		size_t _mapHeight = _map.size();
-	
-		//记录路径的下一个瓦片索引，初始化为传入的起始瓦片
-		SDL_Point _nextIdx = _beginIdx;
-
-		//循环
-		while (true)
-		{
-			//若瓦片索引超出了地图边界，则结束循环
-			if (_nextIdx.x >= _mapWidth || _nextIdx.y >= _mapHeight)
-				break;
-
-			//若路径是否形成闭环，则停止循环，否则放入路径瓦片列表内；第一个被添加进列表的是_beginIdx对应的瓦片索引，之后的点都需要被检验
-			if (CheckRepeatedPointIdx(_nextIdx) == true)
-				break;
-			else
-				tileIdxList.push_back(_nextIdx);
-
-			//下一个砖块到达了家，那么路径结束
-			if (_map[_nextIdx.y][_nextIdx.x].specialLayer == 0)
-				break;
-
-			//默认标记下一个瓦片为存在
-			bool _isNextTileExist = true;
-			
-			//按照瓦片的方向性进行索引的移动
-			switch (_map[_nextIdx.y][_nextIdx.x].directionLayer)
-			{
-			case Tile::TileDir::Up:
-				_nextIdx.y--;
-				break;
-			case Tile::TileDir::Down:
-				_nextIdx.y++;
-				break;
-			case Tile::TileDir::Left:
-				_nextIdx.x--;
-				break;
-			case Tile::TileDir::Right:
-				_nextIdx.x++;
-				break;
-			default:
-				//标记下一个路径瓦片为不存在，并结束switch结构
-				_isNextTileExist = false;
-				break;
-			}
-
-			//如果下一个包含有效路径信息的路径瓦片不存在，跳出while循环
-			if (!_isNextTileExist)
-				break;
-		}
-	}
+	Route(const TileMap&, const SDL_Point&);
 	
 	~Route() = default;
 
-	//对外提供获取路径瓦片索引列表常引用的接口
-	const TileIdxList& GetTileIdxList()
-	{
-		return tileIdxList;
-	}
+	//对外提供获取路径瓦片索引列表常引用的只读接口
+	const TileIdxList& GetTileIdxList();
 
 private:
 	//检查传入的瓦片索引是否已经在路径瓦片列表中出现过，防止路径首尾相接造成构造函数的死循环
-	bool CheckRepeatedPointIdx(const SDL_Point& _targetIdx)
-	{
-		//使用了C++11引入的范围for循环（Range-Based for Loop）进行遍历
-		for (const SDL_Point& i : tileIdxList)
-		{
-			//检测坐标重叠
-			if (i.x == _targetIdx.x && i.y == _targetIdx.y)
-				return true;
-		}
-		return false;
-	}
+	bool CheckRepeatedPointIdx(const SDL_Point&);
 };
+
+Route::Route(const TileMap& _map, const SDL_Point& _beginIdx)
+{
+	//存储瓦片地图的长宽
+	size_t _mapWidth = _map[0].size();
+	size_t _mapHeight = _map.size();
+
+	//记录路径的下一个瓦片索引，初始化为传入的起始瓦片
+	SDL_Point _nextIdx = _beginIdx;
+
+	//循环
+	while (true)
+	{
+		//若瓦片索引超出了地图边界，则结束循环
+		if (_nextIdx.x >= _mapWidth || _nextIdx.y >= _mapHeight)
+			break;
+
+		//若路径是否形成闭环，则停止循环，否则放入路径瓦片列表内；第一个被添加进列表的是_beginIdx对应的瓦片索引，之后的点都需要被检验
+		if (CheckRepeatedPointIdx(_nextIdx) == true)
+			break;
+		else
+			tileIdxList.push_back(_nextIdx);
+
+		//下一个砖块到达了家，那么路径结束
+		if (_map[_nextIdx.y][_nextIdx.x].specialLayer == 0)
+			break;
+
+		//默认标记下一个瓦片为存在
+		bool _isNextTileExist = true;
+
+		//按照瓦片的方向性进行索引的移动
+		switch (_map[_nextIdx.y][_nextIdx.x].directionLayer)
+		{
+		case Tile::TileDir::Up:
+			_nextIdx.y--;
+			break;
+		case Tile::TileDir::Down:
+			_nextIdx.y++;
+			break;
+		case Tile::TileDir::Left:
+			_nextIdx.x--;
+			break;
+		case Tile::TileDir::Right:
+			_nextIdx.x++;
+			break;
+		default:
+			//标记下一个路径瓦片为不存在，并结束switch结构
+			_isNextTileExist = false;
+			break;
+		}
+
+		//如果下一个包含有效路径信息的路径瓦片不存在，跳出while循环
+		if (!_isNextTileExist)
+			break;
+	}
+}
+
+const TileIdxList& Route::GetTileIdxList()
+{
+	return tileIdxList;
+}
+
+bool Route::CheckRepeatedPointIdx(const SDL_Point& _targetIdx)
+{
+	//使用了C++11引入的范围for循环（Range-Based for Loop）进行遍历
+	for (const SDL_Point& i : tileIdxList)
+	{
+		//检测坐标重叠
+		if (i.x == _targetIdx.x && i.y == _targetIdx.y)
+			return true;
+	}
+	return false;
+}
 
 #endif
