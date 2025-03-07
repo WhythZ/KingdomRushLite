556775e995eaab13c1dc6f70ec1ef6e357d9cb48
WhythZ <jichenzong@outlook.com>
2025-03-07 14:16:53 +0800

    03.07 fix status ui render rect

--- KingdomRushLite/Scripts/Source/UI/StatusUI.cpp
+++ KingdomRushLite/Scripts/Source/UI/StatusUI.cpp
@@ -61,45 +61,44 @@ void StatusUI::OnRender(SDL_Renderer* _renderer)
 	//复用的左上顶点坐标
 	static SDL_Point _positionLeftUp = { 0,0 };
 
-	//获取屏幕宽高
-	static const SDL_Point _windowSize = { ConfigManager::Instance()->basicPrefab.windowWidth,
-		ConfigManager::Instance()->basicPrefab.windowHeight };
+	//获取地图Rect（而不是窗口尺寸，因为地图可能与窗口大小不等）
+	static const SDL_Rect _mapRect = ProcessManager::Instance()->mapRect;
 
 	#pragma region MultipleHealthIcon
 	//有多少点血量就渲染一排多少个血量图标
 	for (int _i = 0; _i < ProcessManager::Instance()->GetCurrentHealth(); _i++)
 	{
-		_positionLeftUp.x = 0 + _i * (healthHeartIconBetweenDistance + healthHeartIconSize.x);
-		_positionLeftUp.y = 0;
+		_positionLeftUp.x = _mapRect.x + _i * (healthHeartIconBetweenDistance + healthHeartIconSize.x);
+		_positionLeftUp.y = _mapRect.y;
 		_ui->DrawTexture(_renderer, TextureResID::UI_Icon_HealthHeart, _positionLeftUp, healthHeartIconSize);
 	}
 	#pragma endregion
 
 	#pragma region CoinIcon
 	//金币图标置于血量图标下方一定距离
-	_positionLeftUp.x = 0;
-	_positionLeftUp.y = 0 + healthHeartIconSize.y + rowBetweenDistance;
+	_positionLeftUp.x = _mapRect.x;
+	_positionLeftUp.y = _mapRect.y + healthHeartIconSize.y + rowBetweenDistance;
 	_ui->DrawTexture(_renderer, TextureResID::UI_Icon_Coin, _positionLeftUp, coinIconSize);
 	#pragma endregion
 
 	#pragma region CoinNumText
 	//金币数量的实时更新文本，位置在金币图标右侧一定距离
-	_positionLeftUp.x = 0 + coinIconSize.x + iconTextBetweenDistance;
-	_positionLeftUp.y = 0 + healthHeartIconSize.y + rowBetweenDistance;
+	_positionLeftUp.x = _mapRect.x + coinIconSize.x + iconTextBetweenDistance;
+	_positionLeftUp.y = _mapRect.y + healthHeartIconSize.y + rowBetweenDistance;
 	_ui->DrawTexture(_renderer, coinNumTextTexture, _positionLeftUp, coinNumTextSize);
 	#pragma endregion
 
 	#pragma region WaveNumText
 	//渲染在屏幕右上角
-	_positionLeftUp.x = 0 + _windowSize.x - waveNumTextSize.x;
-	_positionLeftUp.y = 0;
+	_positionLeftUp.x = _mapRect.x + _mapRect.w - waveNumTextSize.x;
+	_positionLeftUp.y = _mapRect.y;
 	_ui->DrawTexture(_renderer, waveNumTextTexture, _positionLeftUp, waveNumTextSize);
 	#pragma endregion
 
 	#pragma region PlayerIcon
 	//将玩家图标渲染在屏幕左下角
-	_positionLeftUp.x = 0;
-	_positionLeftUp.y = 0 + _windowSize.y - playerIconSize.y;
+	_positionLeftUp.x = _mapRect.x;
+	_positionLeftUp.y = _mapRect.y + _mapRect.h - playerIconSize.y;
 	//获取当前玩家种类，绘制对应图标
 	PlayerType _playerType = PlayerManager::Instance()->player->GetType();
 	TextureResID _playerIconTextureID;
@@ -117,14 +116,14 @@ void StatusUI::OnRender(SDL_Renderer* _renderer)
 
 	#pragma region PlayerMpBar
 	//绘制法力值条在玩家头像图标右侧，分别绘制两个技能
-	_positionLeftUp.x = 0 + playerIconSize.x + iconBarBetweenDistance;
-	_positionLeftUp.y = 0 + _windowSize.y - playerIconSize.y;
+	_positionLeftUp.x = _mapRect.x + playerIconSize.x + iconBarBetweenDistance;
+	_positionLeftUp.y = _mapRect.y + _mapRect.h - playerIconSize.y;
 	_ui->DrawDynamicBar(_renderer, _positionLeftUp, mpBarSize, mpBarBorderThickness,
 		mpBarBackgroundColor, mpBarContentColor, skill00MpBarRatio);
 
 	//第二个法力值条在第一个的下方
-	_positionLeftUp.x = 0 + playerIconSize.x + iconBarBetweenDistance;
-	_positionLeftUp.y = (0 + _windowSize.y - playerIconSize.y) + mpBarSize.y + rowBetweenDistance;
+	_positionLeftUp.x = _mapRect.x + playerIconSize.x + iconBarBetweenDistance;
+	_positionLeftUp.y = (_mapRect.y + _mapRect.h - playerIconSize.y) + mpBarSize.y + rowBetweenDistance;
 	_ui->DrawDynamicBar(_renderer, _positionLeftUp, mpBarSize, mpBarBorderThickness,
 		mpBarBackgroundColor, mpBarContentColor, skill01MpBarRatio);
 	#pragma endregion
