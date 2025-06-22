3c0725a919ce40ddee85a530a536ba079277b3c9
WhythZ <jichenzong@outlook.com>
2025-06-22 22:49:19 +0800

    thread safty lazy singleton

--- KingdomRushLite/Scripts/Header/Manager/Manager.hpp
+++ KingdomRushLite/Scripts/Header/Manager/Manager.hpp
@@ -6,9 +6,6 @@
 template <typename M>
 class Manager
 {
-protected:
-	static M* manager;                            //静态的唯一Manager类对象的指针，在外部初始化
-
 public:
 	static M* Instance();                         //静态的函数，用于获取Manager类的唯一实例指针
 
@@ -19,20 +16,13 @@ protected:
 	Manager& operator=(const Manager&) = delete;  //无效化赋值运算符重载的调用
 };
 
-//初始化静态成员变量
-template <typename M>
-M* Manager<M>::manager = nullptr;
-
 //模板函数必须在头文件中实现，不能拆分到另一个源文件中实现
 template <typename M>
 M* Manager<M>::Instance()
 {
-	//若manager未被创建（为空指针），则在堆区创建一个
-	if (!manager)
-		manager = new M();
-
-	//这样我们就可以在外部通过Manager* xxxx = Manager::GetInstance();获取内部这个Manager对象的地址，而不是创建一个新的Manager
-	return manager;
+	//C++11的局部静态变量是线程安全的
+	static M manager;
+	return &manager;
 }
 
 #endif
